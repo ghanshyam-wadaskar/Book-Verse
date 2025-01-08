709b6b5233216478100df2c56fde3927858134ce
Ghanshyam Wadaskar <ghanshyamwadaskar4@gmail.com>
2025-01-08 13:11:31 +0530

    Fix require issue and update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,9 @@
-/** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui";
+
 export default {
-    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-    darkMode: "class",
-    theme: {
-        extend: {},
-    },
-    plugins: [require("daisyui")],
-    daisyui: {
-        themes: [
-            {
-                mytheme: {
-                    primary: "#000000",  // Black for primary elements
-                    secondary: "#ff8b00", // White for secondary elements
-                    accent: "#000000",   // Black accent
-                    neutral: "#00ddff",  // Neutral background
-                    "base-100": "#ebf8fa", // Main background color
-                    "base-content": "#000000", // Main text color
-                },
-            },
-        ],
-    },
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
 };
